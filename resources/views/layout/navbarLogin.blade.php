cfc307cd964226d6f210cf933c1712bc9b3fdf76
khosyrobbin <khosybin@gmail.com>
2022-07-05 14:01:31 +0700

    add template login and dsahboard

---
+++ resources/views/layout/navbarLogin.blade.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg position-absolute top-0 z-index-3 w-100 shadow-none my-3 navbar-transparent mt-4">
+    <div class="container">
+        <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 text-white"
+            href="{{ asset('template') }}/pages/dashboard.html">
+            Argon Dashboard 2
+        </a>
+        <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon mt-2">
+                <span class="navbar-toggler-bar bar1"></span>
+                <span class="navbar-toggler-bar bar2"></span>
+                <span class="navbar-toggler-bar bar3"></span>
+            </span>
+        </button>
+        <div class="collapse navbar-collapse" id="navigation">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a class="nav-link me-2" href="{{ '/register' }}">
+                        <i class="fas fa-user-circle opacity-6  me-1"></i>
+                        Register
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link me-2" href="{{ '/login' }}">
+                        <i class="fas fa-key opacity-6  me-1"></i>
+                        Log In
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
