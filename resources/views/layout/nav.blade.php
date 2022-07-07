f50a9837b1f0ef3a553c74b36f805363c755d522
khosyrobbin <khosybin@gmail.com>
2022-07-07 09:40:08 +0700

    edit navbar

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -9,16 +9,16 @@
     <hr class="horizontal dark mt-0">
     <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
       <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" href="/home">
+        <li class="{{request()->is('/')?'active': ''}}">
+          <a class="nav-link" href="/home">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
             </div>
             <span class="nav-link-text ms-1">Dashboard</span>
           </a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link " href="/tabel">
+        <li class="{{request()->is('tabel')?'active': ''}}">
+          <a class="nav-link" href="/tabel">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i>
             </div>
