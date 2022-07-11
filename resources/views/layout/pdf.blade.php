589a40c28a34681a1ae36203dc01fed7bf0d4571
khosyrobbin <khosybin@gmail.com>
2022-07-11 08:48:49 +0700

    update PDF

--- resources/views/layout/pdf.blade.php
+++ resources/views/layout/pdf.blade.php
@@ -1,46 +1,79 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Membuat Laporan PDF Dengan DOMPDF Laravel</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>LAPORAN MAGANG {{ Auth::user()->name }}</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
+
 <body>
-	<style type="text/css">
-		table tr td,
-		table tr th{
-			font-size: 9pt;
-		}
-	</style>
-	<center>
-		<h5>Membuat Laporan PDF Dengan DOMPDF Laravel</h4>
-		<h6><a target="_blank" href="https://www.malasngoding.com/membuat-laporan-…n-dompdf-laravel/">www.malasngoding.com</a></h5>
-	</center>
+    <div class="id=halaman">
+        <style type="text/css">
+            table tr td,
+            table tr th {
+                font-size: 9pt;
+            }
+        </style>
+        <center>
+            <img src="https://pelindo.co.id/uploads/config/kkX2Ik4l3I5cteU6ZTTKHpbYuWkfNGdBZEhE5lrT.png" alt=""
+                class="navbar-brand-img h-10" width="240" height="70">
+            <h4>LAPORAN ABSEN MAGANG PELINDO</h4>
+        </center>
+        <tr>
+            <h6>Nama : {{ Auth::user()->name }}</h6>
+            <h6>Waktu Magang : 20... s/d 20...</h6>
+        </tr>
+        <table class='table table-bordered'>
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Nama</th>
+                    <th>Deskrisi</th>
+                    <th>Tanggal</th>
+                    <th>Waktu</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $no = 1; ?>
+                @foreach ($absen as $data)
+                    @if ($data->id == Auth::user()->id)
+                        <tr>
+                            <td>{{ $no++ }}</td>
+                            <td>{{ Auth::user()->name }}</td>
+                            <td>{{ $data->deskripsi }}</td>
+                            <td>{{ $data->tanggal }}</td>
+                            <td>{{ $data->waktu }}</td>
+                            <td>{{ $data->status }}</td>
+                        </tr>
+                    @endif
+                @endforeach
+            </tbody>
+        </table>
 
-	<table class='table table-bordered'>
-		<thead>
-			<tr>
-				<th>No</th>
-				<th>Nama</th>
-				<th>Email</th>
-				<th>Alamat</th>
-				<th>Telepon</th>
-				<th>Pekerjaan</th>
-			</tr>
-		</thead>
-		<tbody>
-			{{-- @php $i=1 @endphp
-			@foreach($pegawai as $p)
-			<tr>
-				<td>{{ $i++ }}</td>
-				<td>{{$p->nama}}</td>
-				<td>{{$p->email}}</td>
-				<td>{{$p->alamat}}</td>
-				<td>{{$p->telepon}}</td>
-				<td>{{$p->pekerjaan}}</td>
-			</tr>
-			@endforeach --}}
-		</tbody>
-	</table>
+        <p>menyatakan bahwa yang namanya tertera diatas mealukan magang di PT. Pelindo Multi Terminal.</p>
 
+        <div style="width: 30%; text-align: left; float: right;">Banyuwangi, <span id="tanggalwaktu"></span></div><br>
+        <div style="width: 30%; text-align: left; float: right;">Yang bertanda tangan,</div><br><br><br><br><br>
+        <div style="width: 30%; text-align: left; float: right;">Branch Manager</div>
+
+        <script>
+            var tw = new Date();
+            if (tw.getTimezoneOffset() == 0)(a = tw.getTime() + (7 * 60 * 60 * 1000))
+            else(a = tw.getTime());
+            tw.setTime(a);
+            var tahun = tw.getFullYear();
+            var hari = tw.getDay();
+            var bulan = tw.getMonth();
+            var tanggal = tw.getDate();
+            var hariarray = new Array("Minggu,", "Senin,", "Selasa,", "Rabu,", "Kamis,", "Jum'at,", "Sabtu,");
+            var bulanarray = new Array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September",
+                "Oktober", "Nopember", "Desember");
+            document.getElementById("tanggalwaktu").innerHTML = hariarray[hari] + " " + tanggal + " " + bulanarray[bulan] +
+                " " + tahun;
+        </script>
+    </div>
 </body>
+
 </html>
