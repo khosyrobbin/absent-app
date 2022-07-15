a19a5996204325a5cec7339cee4f8236faaa98d9
khosyrobbin <khosybin@gmail.com>
2022-07-15 14:49:11 +0700

    test time_zone

--- database/migrations/2022_07_06_012505_create_absens_table.php
+++ database/migrations/2022_07_06_012505_create_absens_table.php
@@ -16,8 +16,8 @@ class CreateAbsensTable extends Migration
     {
         Schema::create('absens', function (Blueprint $table) {
             $table->id('id_absen');
-            $table->date('tanggal')->time_zone('Asia/Jakarta');
-            $table->time('waktu')->time_zone('Asia/Jakarta');
+            $table->date('tanggal')->setlocale('id')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->time('waktu')->setlocale('id')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->string('deskripsi');
             $table->string('status');
             $table->timestamps();
